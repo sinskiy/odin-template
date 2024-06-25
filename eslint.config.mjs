6e63dea24bb0f3c9f7feae2173ce7b1d27ddcf34
sinskiy <sinskiy@proton.me>
2024-06-25 08:28:34 +0300

    fix: config types  (?)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.js"],
+    languageOptions: { globals: globals.browser, sourceType: "module" },
+  },
+  {
+    files: ["*.config.js"],
+    languageOptions: { globals: globals.node, sourceType: "commonjs" },
+  },
   pluginJs.configs.recommended,
 ];
